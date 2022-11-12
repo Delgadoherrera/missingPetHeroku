4bd92df1ee5b36412d756728ca56b14828bd50cc
Delgadoherrera <delgado.luciano.herrera@gmail.com>
2022-11-12 03:43:44 -0300

    cambiando confi sequelize

--- backend/src/database/models/index.js
+++ backend/src/database/models/index.js
@@ -8,12 +8,10 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable])[env];
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+const URI = `${process.env.DB_DIALECT}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+const sequelize = new Sequelize(URI);
 
 fs
   .readdirSync(__dirname)
