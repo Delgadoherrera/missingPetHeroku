263b5a730d94373418ca6e1cf1aa19146ac5a31a
Delgadoherrera <delgado.luciano.herrera@gmail.com>
2022-11-12 01:07:19 -0300

    config js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,5 +7,21 @@ module.exports = {
     'host': process.env.DB_HOST,
     'port': process.env.DB_PORT,
     'dialect': process.env.DB_DIALECT
+  },
+  "test": {
+    'username': process.env.DB_USERNAME,
+    'passowrd': process.env.DB_PASSWORD,
+    'database': process.env.DB_DATABASE,
+    'host': process.env.DB_HOST,
+    'port': process.env.DB_PORT,
+    'dialect': process.env.DB_DIALECT
+  },
+  "production": {
+    'username': process.env.DB_USERNAME,
+    'passowrd': process.env.DB_PASSWORD,
+    'database': process.env.DB_DATABASE,
+    'host': process.env.DB_HOST,
+    'port': process.env.DB_PORT,
+    'dialect': process.env.DB_DIALECT
   }
 }
