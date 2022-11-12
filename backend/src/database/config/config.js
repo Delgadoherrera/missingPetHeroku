a51e7f33938c5c67305b7b1a35fad7ae3f39fca4
Delgadoherrera <delgado.luciano.herrera@gmail.com>
2022-11-12 03:57:31 -0300

    yandaa

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
-    "password": t0zcw2kkuc53bb6e,
+    "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
